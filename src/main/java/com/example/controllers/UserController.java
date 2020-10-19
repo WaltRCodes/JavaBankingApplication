49a2ea2b2a0f4ff34bcb3d01f9ba6618408edb66
Walter <4wrada@gmail.com>
2020-10-19 11:09:34 -0400

    commented the user controller file

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -6,18 +6,18 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.example.dao.RoleDAOImpl;
 import com.example.dao.UserDAOImpl;
-import com.example.models.Role;
 import com.example.models.User;
 
 public class UserController {
-
+	//this controller handles changes to user account information
 	public static void update(HttpServletRequest request, HttpServletResponse response,User u) throws IOException {
+		//this PrintWriter object is used to create HTML responses once certain request are called
 		PrintWriter pw = response.getWriter();
 		String [] str = request.getRequestURI().split("/");
+		//first I check if the request is a POST request
 		if(request.getMethod().equals("POST")) {
-			
+				//if it is then I capture the values of the form fields and match them to the keys in the user object
 				u.setUsername(request.getParameter("username"));
 				u.setPassword(request.getParameter("password"));
 				u.setFirstName(request.getParameter("first"));
@@ -31,20 +31,22 @@ public class UserController {
 //						u.setRole(r);
 //					}
 //				}
+				//I then use the UserDAO to push the updated user object into the respective entry in the database
 				UserDAOImpl udao = new UserDAOImpl();
 				udao.updateUser(u);
-				
+				//I let the user know that there account was successfully updated and redirect them to a new page
 				pw.write("<p>Account was updated successfully</p>");
 				response.sendRedirect("http://localhost:8080/rocp-project/Users/");
 			
 		} else {
-			
+			//if any other of request rather than a post was sent, I first check if there was a user logged into the session
 			if(request.getSession(false)==null) {
+				//if not, I send an error
 				System.out.println("There was no user logged into the session");
 				response.sendError(400,"There was no user logged into the session");
 			} else {
 				//response.sendRedirect("http://localhost:8080/SignInMaven/Success.html");
-				
+				//If a user is logged in, I print out the form that is populated with the user info and can be used to modify the account
 				
 				pw.write("<h1>Here is your Profile info</h1>\r\n" + 
 						"<form action=\"/rocp-project/Users/"+str[3]+"\" method=\"post\">\r\n" + 
