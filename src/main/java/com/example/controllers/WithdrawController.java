b95ebacafd209853888b95e4485d91a780fac92b
Walter <4wrada@gmail.com>
2020-10-20 00:27:51 -0400

    updated the Withdraw Controller with comments

--- src/main/java/com/example/controllers/WithdrawController.java
+++ src/main/java/com/example/controllers/WithdrawController.java
@@ -16,27 +16,37 @@ import com.example.models.AccountType;
 public class WithdrawController {
 
 	public static void withdraw(HttpServletRequest request, HttpServletResponse response,int accountnum) throws IOException {
+		//this is what will be used to print HTML tags as a response to request
 		PrintWriter pw = response.getWriter();
 		String [] str = request.getRequestURI().split("/");
 		if(request.getMethod().equals("POST")) {
 			try {
-			Double amt = Double.parseDouble(request.getParameter("amount"));
-			AccountDAOImpl adao = new AccountDAOImpl();
-			Account a = adao.selectAccountById(accountnum);
-			AccountStatusDAOImpl asdao = new AccountStatusDAOImpl();
-			AccountTypeDAOImpl atdao = new AccountTypeDAOImpl();
-			AccountStatus as = asdao.selectAccountStatusById(a.getStatus().getStatusId());
-			a.setStatus(as);
-			AccountType at = atdao.selectAccountTypeById(a.getType().getTypeId());
-			a.setType(at);
-			a.withdraw(amt);
-			//account deposit logic
-			adao.updateAccount(a);
-			pw.write("<p>Withdraw was successful</p>");
-			response.sendRedirect("http://localhost:8080/rocp-project/Accounts");
+				//this retrieves the amount to be withdrawn from the account
+				Double amt = Double.parseDouble(request.getParameter("amount"));
+				AccountDAOImpl adao = new AccountDAOImpl();
+				//this retrieves the account info from the database and inputs it into an account object
+				Account a = adao.selectAccountById(accountnum);
+				AccountStatusDAOImpl asdao = new AccountStatusDAOImpl();
+				AccountTypeDAOImpl atdao = new AccountTypeDAOImpl();
+				//this retrieves the latest status details tied to the account into an AccountStatus object 
+				AccountStatus as = asdao.selectAccountStatusById(a.getStatus().getStatusId());
+				//this updates the account object with the retrieved AccountStatus object
+				a.setStatus(as);
+				//this retrieves the AccountType info based on what's in the account object
+				AccountType at = atdao.selectAccountTypeById(a.getType().getTypeId());
+				//this sets the new AccountType into the account
+				a.setType(at);
+				//this withdraws the amount from the account
+				a.withdraw(amt);
+				//this sends the updated account object to be used in order to update the entries in the database
+				adao.updateAccount(a);
+				//if the update is successful then a success message is printed prior to a redirect 
+				pw.write("<p>Withdraw was successful</p>");
+				response.sendRedirect("http://localhost:8080/rocp-project/Accounts");
 			} catch(Exception e) {
+				//if the account cannot be updated an error and the form are printed onto the page
 				pw.write("<p>There was an issue processing your transaction</p>\r\n" + 
-			"<p>Please input the amount you would like to withdraw</p>\r\n" + 
+						"<p>Please input the amount you would like to withdraw</p>\r\n" + 
 						"<form action=\"/rocp-project/Accounts/"+str[3]+"/"+str[4]+"\" method=\"post\">\r\n" + 
 						"		Amount: <input type = \"number\" name  = \"amount\" >\r\n" + 
 						"		<button type=\"submit\" >Submit</button>\r\n" + 
@@ -44,7 +54,7 @@ public class WithdrawController {
 			}
 			
 		}else {
-			
+			//if the request is not a post request then the widraw form is outputed at this page
 			pw.write("<p>Please input the amount you would like to withdraw</p>\r\n" + 
 					"<form action=\"/rocp-project/Accounts/"+str[3]+"/"+str[4]+"\" method=\"post\">\r\n" + 
 					"		Amount: <input type = \"number\" name  = \"amount\" >\r\n" + 
