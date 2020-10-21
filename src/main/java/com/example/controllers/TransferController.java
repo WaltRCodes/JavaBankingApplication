a6ca0f22c777f9070922c5e7e49ad285ca36a420
Walter <4wrada@gmail.com>
2020-10-21 01:23:33 -0400

    commented the transfer controller file

--- src/main/java/com/example/controllers/TransferController.java
+++ src/main/java/com/example/controllers/TransferController.java
@@ -16,34 +16,41 @@ import com.example.models.AccountType;
 public class TransferController {
 
 	public static void transfer(HttpServletRequest request, HttpServletResponse response,int accountnum) throws IOException {
+		//the PrintWriter object prints HTML takes as a response from request
 		PrintWriter pw = response.getWriter();
 		String [] str = request.getRequestURI().split("/");
 		if(request.getMethod().equals("POST")) {
 			try {
-			Double amt = Double.parseDouble(request.getParameter("amount"));
-			AccountDAOImpl adao = new AccountDAOImpl();
+				//this draws the amount needed to be transfered into between accounts
+				Double amt = Double.parseDouble(request.getParameter("amount"));
+				AccountDAOImpl adao = new AccountDAOImpl();
+				//This draws the user account entry into a new account object
+				Account a = adao.selectAccountById(accountnum);
+				AccountStatusDAOImpl asdao = new AccountStatusDAOImpl();
+				AccountTypeDAOImpl atdao = new AccountTypeDAOImpl();
+				//This draws the updated AccountStatus and AccountType into the account object
+				AccountStatus as = asdao.selectAccountStatusById(a.getStatus().getStatusId());
+				a.setStatus(as);
+				AccountType at = atdao.selectAccountTypeById(a.getType().getTypeId());
+				a.setType(at);
+				//this draws the transfer account info into an account object and then pulls the respective AccountType and AccountStatus
+				Account transfer = adao.selectAccountById(Integer.parseInt(request.getParameter("transferaccount")));
+				AccountStatus ast = asdao.selectAccountStatusById(transfer.getStatus().getStatusId());
+				transfer.setStatus(ast);
+				AccountType att = atdao.selectAccountTypeById(transfer.getType().getTypeId());
+				transfer.setType(att);
+				//this initiates the transfer between two account objects
+				a.transfer(amt,transfer);
 			
-			Account a = adao.selectAccountById(accountnum);
-			AccountStatusDAOImpl asdao = new AccountStatusDAOImpl();
-			AccountTypeDAOImpl atdao = new AccountTypeDAOImpl();
-			AccountStatus as = asdao.selectAccountStatusById(a.getStatus().getStatusId());
-			a.setStatus(as);
-			AccountType at = atdao.selectAccountTypeById(a.getType().getTypeId());
-			a.setType(at);
-			
-			Account transfer = adao.selectAccountById(Integer.parseInt(request.getParameter("transferaccount")));
-			AccountStatus ast = asdao.selectAccountStatusById(transfer.getStatus().getStatusId());
-			transfer.setStatus(ast);
-			AccountType att = atdao.selectAccountTypeById(transfer.getType().getTypeId());
-			transfer.setType(att);
-			a.transfer(amt,transfer);
-			
-			//account deposit logic
-			adao.updateAccount(a);
-			adao.updateAccount(transfer);
-			pw.write("<p>Transfer was successful</p>");
-			response.sendRedirect("http://localhost:8080/rocp-project/Accounts");
+				//this updates the user account
+				adao.updateAccount(a);
+				//this updates the transfer account
+				adao.updateAccount(transfer);
+				//this outputs a success message to the user to show before redirecting them
+				pw.write("<p>Transfer was successful</p>");
+				response.sendRedirect("http://localhost:8080/rocp-project/Accounts");
 			} catch(Exception e) {
+				//this outputs an error message with the form so users can try again
 				pw.write("<p>There was an issue processing your transaction</p>\r\n" + 
 						"<p>Please input the amount you would like to transfer and the account number of the recipient</p>\r\n" + 
 						"<form action=\"/rocp-project/Accounts/"+str[3]+"/"+str[4]+"\" method=\"post\">\r\n" + 
@@ -54,7 +61,7 @@ public class TransferController {
 			}
 			
 		}else {
-			
+			//this outputs a form used to transfer money between accounts
 			pw.write("<p>Please input the amount you would like to transfer and the account number of the recipient</p>\r\n" + 
 					"<form action=\"/rocp-project/Accounts/"+str[3]+"/"+str[4]+"\" method=\"post\">\r\n" + 
 					"		Transfer to: <input type = \"text\" name  = \"transferaccount\">\r\n" + 
